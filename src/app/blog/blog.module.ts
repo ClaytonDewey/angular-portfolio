122136e3fc513c8791f0e644ab24e53b1f353d29
ClaytonDewey <clay@dryadmedia.com>
2022-03-31 12:26:03 -0500

    Updated paths for blog component...

--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -2,7 +2,7 @@ import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {ScullyLibModule} from '@scullyio/ng-lib';
 import {BlogRoutingModule} from './blog-routing.module';
-import {BlogComponent} from './blog.component';
+import {BlogComponent} from './blog/blog.component';
 
 @NgModule({
   declarations: [BlogComponent],
