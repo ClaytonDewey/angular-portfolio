53232f4c81c1ad57d38b679afd191545af22fe84
ClaytonDewey <clay@dryadmedia.com>
2022-03-31 10:40:49 -0500

    Added HeaderComponent to core exports...

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -15,6 +15,7 @@ import { HeaderComponent } from './header/header.component';
     RouterModule,
     NgbModule,
     HttpClientModule
-  ]
+  ],
+  exports: [ HeaderComponent ]
 })
 export class CoreModule { }
